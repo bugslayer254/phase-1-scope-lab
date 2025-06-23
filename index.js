34eb0c5e9b60b8e9319001bcef1d90180a504909
bugslayer254 <kelgraygraykel@gmail.com>
2025-06-23 15:53:11 +0300

    scopes

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
+
 var customerName = 'bob';
 
-// Function that uppercases the global customerName variable
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
 
-// Function that creates a global variable bestCustomer and assigns it 'not bob'
 function setBestCustomer() {
-  bestCustomer = 'not bob'; // Declaring without var/let/const makes it global (not recommended normally)
+  bestCustomer = 'not bob';
 }
 function overwriteBestCustomer() {
   bestCustomer = 'maybe bob';
 }
-const leastFavoriteCustomer = 'Karen';
+const leastFavoriteCustomer = 'some customer';
 
-// Function that attempts to change the value of the constant
 function changeLeastFavoriteCustomer() {
-  // This will throw a TypeError because you're trying to reassign a const
-  leastFavoriteCustomer = 'Not Karen'; 
+  leastFavoriteCustomer = 'new customer'; 
 }
